94f4896be971914aa57559c62d107d4c6c3f5192
Bruno Bortoli <bruno.bortoli@nsc.com.br>
2022-08-11 10:24:55 -0300

    chore: Adicionando nova lógica de datas; fix: correção do bug de um dia a menos; style: novo layte de datas; add: pacotes bootstrap add;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { AppProvider } from '../data/context/AppContext'
 import { AuthProvider } from '../data/context/AuthContext'
 import '../styles/globals.css'
@@ -6,7 +7,26 @@ function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
       <AuthProvider>
-        <Component {...pageProps} />
+        <>
+          <Head>
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+              integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+              crossorigin="anonymous"
+            />
+            <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+
+            <script
+              src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+              crossorigin></script>
+
+            <script
+              src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+              crossorigin></script>
+          </Head>
+          <Component {...pageProps} />
+        </>
       </AuthProvider>
     </AppProvider>
   )
--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Alert, Card, Form } from "react-bootstrap";
 
 export default function Teste() {
 
     const [data, setData] = useState('');
-    // const [daySelina, setDaySelina] = useState('');
+    const [juliana, setJuliana] = useState('');
+
 
     function dataSelina() {
         let date: Date = new Date(data);
         const day = (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
         let teste = (date.getFullYear() - 1900) + '' + day.toString().padStart(3, '0');
-        // setDaySelina(teste);
-        return (date.getFullYear() - 1900) + '' + day.toString().padStart(3, '0');
+        let number: number = + teste;
+        return number + 1;
     }
 
     function julianDateSixToGregorianDate() {
-        // var teste:number = + daySelina; 
-        var number = 0 + dataSelina();
+        var number: number = + juliana;
         let julianYear = number.toString().slice(0, 3);
         let julianDay = number.toString().slice(3, 6);
         let date = new Date(1900 + parseInt(julianYear), 0, 0, 0, 0, 0)
@@ -32,10 +36,54 @@ export default function Teste() {
 
 
     return (
-        <div>
-            <input type="date" onChange={e => setData(e.target.value)} /> <br></br>
-            <h1>{dataSelina()}</h1>
-            <h1> { julianDateSixToGregorianDate(12) }</h1>
-        </div>
+        <Container fluid>
+            <Row className="justify-content-md-center">
+                <Col xs={6}>
+                    <Card>
+                        <Card.Header>
+                            <h1>Converter Datas</h1>
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>Por favor escolha uma Data para Juliana</Form.Label>
+                                        <Form.Control type="date" onChange={e => setData(e.target.value)} />
+                                    </Form.Group>
+                                </Form>
+                            </Row>
+                            <br />
+                            {data ? (
+                                <Row>
+                                    <Col>
+                                        <Alert variant="success">Valor em data Juliana é : {dataSelina()}</Alert>
+                                    </Col>
+                                </Row>
+                            ) : (<></>)}
+
+                            <hr />
+                            <br />
+
+                            <Row>
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>Por favor informe a data Juliana para converter</Form.Label>
+                                        <Form.Control type="number" onChange={e => setJuliana(e.target.value)} />
+                                    </Form.Group>
+                                    <br/>
+                                    {juliana ? (
+                                        <Row>
+                                            <Col>
+                                                <Alert variant="primary">Valor em data é : {julianDateSixToGregorianDate()}</Alert>
+                                            </Col>
+                                        </Row>
+                                    ) : (<></>)}
+                                </Form>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
